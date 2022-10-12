4506296f50943cc6960bde24be1f00e8ac8d02ed
ixueyi <98458415+ixueyi@users.noreply.github.com>
2022-10-12 15:48:47 +0800

    Update install_terminal.sh
    change the installing oh-my-zsh url to a mirrors url

--- install_terminal.sh
+++ install_terminal.sh
@@ -3,4 +3,4 @@ set -eux pipefail
 
 # Install ZSH
 sudo apt install -y git-core zsh curl
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh//raw/master/tools/install.sh)"
